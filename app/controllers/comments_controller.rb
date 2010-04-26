84dbb02804ca77c7ef3ee2a9e2862664debc690d
dani <danigb@gmail.com>
2010-04-26 14:34:40 +0200

    edit comments and videos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < AdminController
   inherit_resources
   belongs_to :section
+
+  def update
+    update! { section_comments_path(@section)}
+  end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,12 @@
 class VideosController < AdminController
   inherit_resources
   belongs_to :section
+
+  def update
+    update! { section_videos_path(@section)}
+  end
+
+  def create
+    create! { section_videos_path(@section)}
+  end
 end

c2a3faa1bd38031068ae56a5864cdff2289aba6c
dani <danigb@gmail.com>
2010-05-25 09:55:11 +0200

    pantallas montadas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,21 @@ class PagesController < ApplicationController
 
   def section
     @section = Section.find_by_name(params[:id])
+    @video = @section.videos.first
     render :action => @section.name
   end
+
+  def intro
+    @section = Section.find_by_name('intro')
+    @video = Video.find(params[:id])
+    render :action => 'intro'
+  end
+
+  def acciones
+    @section = Section.find_by_name('acciones')
+    @video = Video.find(params[:id])
+    render :action => 'acciones'
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => 'pages', :action => 'index'
 
-  # FIXME
   ['intro', 'acciones', 'tutorial', 'faq'].each do |name|
     map.send(name.to_sym, "/#{name}", :controller => 'pages', :action => 'section', :id => name)
   end
+  map.connect '/intro/:id', :controller => 'pages', :action => 'intro'
+  map.connect '/acciones/:id', :controller => 'pages', :action => 'acciones'
 
   map.resources( :sections, :as => 'secciones', :path_prefix => 'admin' ) do |section|
     section.resources :videos

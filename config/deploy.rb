80bf1ce93e4c963e0f26eca8bb270330b5cab8c9
danigb <danigb@gmail.com>
2010-07-15 10:56:45 +0200

    mierda

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ after "deploy", "deploy:cleanup"
 namespace :config do
   desc "copy shared configurations to current"
   task :copy_shared_configurations, :roles => [:app] do
+    run "ln -sf #{shared_path}/cache #{release_path}/public/cache"
     %w[database.yml].each do |f|
       run "ln -nsf #{shared_path}/config/#{f} #{release_path}/config/#{f}"
     end
